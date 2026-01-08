70a1067309a65881ac5db95e9db397a714a8cd76
qwer0114 <rnalgh0114@gmail.com>
2026-01-08 17:44:13 +0900

    feat: 라이트 모드, 다크모드 대응

---
+++ app/layout.tsx
@@ -0,0 +1,33 @@
+import Header from "@/components/layout/header/header";
+import "./globals.css";
+import localFont from "next/font/local";
+import { ThemeProvider } from "@/components/providers/theme-provider";
+
+const pretendard = localFont({
+  src: "../public/fonts/PretendardVariable.woff2",
+  display: "swap",
+  weight: "45 920",
+  variable: "--font-pretendard",
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={`bg-background ${pretendard.variable} font-pretendard`}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Header />
+          <main>{children}</main>
+        </ThemeProvider>
+      </body>
+    </html>
+  );
+}
---
+++ components/layout/header/mode-toggle.tsx
@@ -0,0 +1,49 @@
+"use client";
+import { Moon, Sun } from "lucide-react";
+import { useTheme } from "next-themes";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+
+function ModeToggle() {
+  const Menus = [
+    {
+      theme: "light",
+      display: "Light",
+    },
+    {
+      theme: "dark",
+      display: "Dark",
+    },
+    {
+      theme: "system",
+      display: "System",
+    },
+  ];
+  const { setTheme } = useTheme();
+
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" size="icon">
+          <Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
+          <Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+          <span className="sr-only">Toggle theme</span>
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        {Menus.map(({ theme, display }) => (
+          <DropdownMenuItem key={theme} onClick={() => setTheme(theme)}>
+            {display}
+          </DropdownMenuItem>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+}
+
+export default ModeToggle;
